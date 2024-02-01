f09c8e15c55a457b3b4c215e7129e89b0ea4c2e8
eya46 <644310932@qq.com>
2024-02-01 19:56:46 +0800

    fix bug x5

--- nonebot_plugin_playwright/browser.py
+++ nonebot_plugin_playwright/browser.py
@@ -5,7 +5,7 @@ import os
 from time import time
 
 from nonebot import get_driver, logger
-from playwright.async_api import async_playwright, Playwright, Browser
+from playwright.async_api import async_playwright, Playwright, Browser, Error
 
 from .config import config
 
@@ -38,27 +38,39 @@ async def browser_init(**kwargs):
 def playwright_install(browser: str, download_host: str = config.playwright_download_host):
     os.environ["PLAYWRIGHT_DOWNLOAD_HOST"] = download_host
     logger.opt(colors=True).info(f"<y>Installing {browser}...</y>")
-    os.system(f"playwright install {browser}")
-    logger.opt(colors=True).info("<y>Installing dependencies...</y>")
-    os.system("playwright install-deps")
+    from playwright.__main__ import main as playwright_main
+    import sys
+    sys.argv = ["", "install", browser, "--with-deps"]
+    try:
+        playwright_main()
+    except SystemExit as e:
+        if e.code == 0:
+            logger.info("playwright install success")
+        else:
+            logger.error("playwright install failed")
 
 
 @_driver.on_startup
 async def playwright_init():
     global _PLAYWRIGHT
-    _PLAYWRIGHT = await async_playwright().start()
-    if config.playwright_executable_path is None:
-        playwright_install(config.playwright_browser)
-    else:
-        logger.opt(colors=True).info(f"<y>Playwright executable path: {config.playwright_executable_path}</y>")
-    logger.opt(colors=True).info(f"<y>Playwright started: {config.playwright_browser}</y>")
     default_kwargs = {
         "channel": config.playwright_browser,
         "headless": config.playwright_headless,
         "executable_path": config.playwright_executable_path
     }
     default_kwargs.update(config.playwright_extra_kwargs)
-    await browser_init(**default_kwargs)
+    _PLAYWRIGHT = await async_playwright().start()
+    try:
+        await browser_init(**default_kwargs)
+    except Error as e:
+        if config.playwright_executable_path is None:
+            playwright_install(config.playwright_browser)
+            await browser_init(**default_kwargs)
+        else:
+            raise e
+    if config.playwright_executable_path:
+        logger.opt(colors=True).info(f"<y>Playwright executable path: {config.playwright_executable_path}</y>")
+    logger.opt(colors=True).info(f"<y>Playwright started: {config.playwright_browser}</y>")
 
 
 async def playwright_close():
